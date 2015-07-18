7be0083fd1e492f8c88ad545e69fb75ea6a1ca41
Michael Simpsom <msimpson8mic@gmail.com>
2015-07-17 21:25:42 -0400

    Added functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+	$('.input').blur();
+	$('.add-button').click(function() {
+		var text = $('.item').val();
+		$('.checklist-list').append( '<li>' + '<input type="checkbox" name="checkbox" />' + '<label for="item">' + text + '</label>' + '</li>');
+		$('.item').val('');
+	});
 	
+	$('.remove-button').click(function(){
+		$('input[type="checkbox"]:checked').each(function() {
+			$(this).parent("li").remove();
+		});
+	});
 });
